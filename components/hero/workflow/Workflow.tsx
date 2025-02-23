8324e8b02e18e21f4555c801fce936f1d0e046f0
JeanCharlie <santanajeankarlo@gmail.com>
2025-02-23 13:03:30 +0100

    feat: replace "Ver más" button with Link component to navigate to About Us page

--- components/hero/workflow/Workflow.tsx
+++ components/hero/workflow/Workflow.tsx
@@ -2,31 +2,37 @@
 import { Container } from "@/components/ui/Container";
 import Stats from "@/components/ui/stats/Stats";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Workflow() {
   return (
     <section className="relative pb-16 md:pb-24 bg-gradient-to-br from-[#085078] to-[#4aa59a]">
-      
-      <div className="absolute inset-0" style={{
-        clipPath: "polygon(25% 0%, 100% 5%, 100% 95%, 75% 100%, 0 97%, 0 3%)",
-        background: 'linear-gradient(135deg, rgba(8,80,120,0.95) 0%, rgba(74,165,154,0.95) 100%)'
-      }}></div>
+      <div
+        className="absolute inset-0"
+        style={{
+          clipPath: "polygon(25% 0%, 100% 5%, 100% 95%, 75% 100%, 0 97%, 0 3%)",
+          background:
+            "linear-gradient(135deg, rgba(8,80,120,0.95) 0%, rgba(74,165,154,0.95) 100%)",
+        }}
+      ></div>
       <Container className="relative z-10">
-      {/* <div className="container mx-auto px-4 relative z-10"> */}
+        {/* <div className="container mx-auto px-4 relative z-10"> */}
         <div className="flex flex-col lg:flex-row items-center justify-between gap-12">
           <div className="w-full lg:w-1/2 space-y-8">
             <h2 className="font-montserrat text-5xl text-white font-bold leading-tight">
               Elevando su productividad con soluciones versátiles
             </h2>
-            
+
             <p className="text-white/90 text-2xl  leading-relaxed">
-              Optimizamos sus procesos empresariales mediante soluciones 
-              tecnológicas adaptadas a sus necesidades específicas, 
-              garantizando eficiencia y escalabilidad.
+              Optimizamos sus procesos empresariales mediante soluciones
+              tecnológicas adaptadas a sus necesidades específicas, garantizando
+              eficiencia y escalabilidad.
             </p>
-            <button className="text-2xl mt-8 bg-white  text-[#085078] hover:bg-[#4aa59a] transition-colors duration-300 font-semibold py-3 px-8 rounded-lg shadow-md hover:shadow-lg transform hover:-translate-y-1">
-            Ver más
-          </button>
+            <Link href="/about-us">
+              <button className="text-2xl mt-8 bg-white  text-[#085078] hover:bg-[#4aa59a] transition-colors duration-300 font-semibold py-3 px-8 rounded-lg shadow-md hover:shadow-lg transform hover:-translate-y-1">
+                Ver más
+              </button>
+            </Link>
 
             <div className="mt-12">
               <Stats />
@@ -44,7 +50,7 @@ export default function Workflow() {
             />
           </div>
         </div>
-      {/* </div> */}
+        {/* </div> */}
       </Container>
     </section>
   );

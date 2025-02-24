c1520c6d633f1ae7f67030664b3838a0ace8f168
JeanCharlie <santanajeankarlo@gmail.com>
2025-02-24 18:33:59 +0100

    feat: replace BlogLocal component with TechNews and implement blog post filtering

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,9 @@
-import BlogLocal from "@/components/pages/blog/BlogLocal";
-
+import TechNews from "@/components/pages/blog/TechNews";
 
 export default function BlogPage() {
   return (
     <main className="venice-blue">
-      <BlogLocal />
+      <TechNews />
     </main>
   );
 }

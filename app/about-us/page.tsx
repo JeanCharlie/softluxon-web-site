4a6321aaa0913b8a9115cc4a24c3ad74d058f7ba
JeanCharlie <santanajeankarlo@gmail.com>
2025-02-24 17:59:40 +0100

    feat: rename ContactPage component to AboutUsPage and clean up BlogPage

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,6 +1,6 @@
 import AboutUs from "@/components/pages/about-us/AboutUs";
 
-export default function ContactPage() {
+export default function AboutUsPage() {
   return (
     <main className="venice-blue">
       <AboutUs />
